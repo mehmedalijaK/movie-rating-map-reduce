32bcfd9ad3b3cfc76c6abee2e84dd3a8789c0caf
mehmedalijaK <rg.mehmedalija@gmail.com>
2023-12-12 00:41:07 +0100

    implement average rating for each movie

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json
+from functools import reduce
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def load_data(file_path):
+    with open(file_path) as my_file:
+        return json.load(my_file)
 
 
-# Press the green button in the gutter to run the script.
+def map_data(user_rating_list):
+    return map(lambda value: (value['movie_id'], value['rating'], 1), user_rating_list)
+
+
+def shuffle_data(movie_rating_list):
+    return sorted(movie_rating_list, key=lambda x: x[0])
+
+
+def reduce_sum(acc, x):
+    if acc and acc[-1][0] == x[0]:
+        acc[-1] = (acc[-1][0], acc[-1][1] + x[1], acc[-1][2] + 1)
+    else:
+        acc.append(x)
+    return acc
+
+
+def reduce_data(shuffled_list):
+    return reduce(reduce_sum, shuffled_list, [])
+
+
+def reduce_average(final_data):
+    return map(lambda x: ("movie_id " + str(x[0]), round(x[1] / x[2], 2)), final_data)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    data = load_data('sample_data/user_reviews.json')
+    mapped_data = map_data(data)
+    shuffled_data = shuffle_data(mapped_data)
+    reduced_data = reduce_data(shuffled_data)
+    average_result = reduce_average(reduced_data)
+    print(list(average_result))
